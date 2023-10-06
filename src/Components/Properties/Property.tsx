839ddbd4e8d148f7ade8082b448be295cca5e015
sarmistha.mohanty <sarmistha.mohanty@cloudera.com>
2023-10-06 15:46:46 +0530

    Added types for property

---
+++ src/Components/Properties/Property.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { property } from "../types";
+
+type Props = {
+    prop : property,
+}
+const Property : React.FC<Props> = (props) =>{
+    const { prop } = props;
+     return <>
+     
+     </>
+
+}
+
+export default Property;
--- src/Components/types.ts
+++ src/Components/types.ts
@@ -1,8 +1,24 @@
 
 export type User = {
-    username : String,
-    email : String,
-    password : String,
-    phoneNo : String | Number
+    username : string,
+    email : string,
+    password : string,
+    phoneNo : string | Number
 }
 
+export type property = {
+    propertyId: string,
+    propertyDescription? : string, 
+    propertyName? : string,
+    propertAddress? : string,
+    propertyPrice? : Number,
+    propertyOwner? : string,
+    propertyImage? : ImageData
+}
+
+export enum propertyCategories {
+    MOST_POPULAR = "most popular",
+    BY_BUDGET = "By Budget",
+    BY_PROPERTY_TYPE = "By Property Type",
+    BY_BHK = "By BHK"
+}
