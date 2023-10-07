efb50add3665c127e4f052211c3c4e3fc7b421e8
an1sh07 <anishasutosh07@gmail.com>
2023-10-08 04:27:20 +0530

    improved login signup page

---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+   background-color : blue
+`
+
+
+const LandingPage = () => {
+    
+
+    return <>
+    <StyledDiv>
+     <h1>Hello</h1>
+     </StyledDiv>
+    </>
+}
+
+export default LandingPage;
+
+
+// <LandingPage/>
